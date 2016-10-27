23970830c7d75d75e9fab49a94432fb647fa3be6
OvsyannikovAU <ovsyannikov.a.u@gmail.com>
2016-10-27 21:30:54 +0300

    Примеры из 4 он-лайн урока

--- CrashProtect.cs
+++ CrashProtect.cs
@@ -8,10 +8,38 @@ namespace Com.CodeGame.CodeRacing2015.DevKit.CSharpCgdk
 {
     class CrashProtect
     {
+        private int steps=0;
+        private double oldX = 0;
+        private double oldY = 0;
+        private int deltaPorog = 150; //необходимо подобрать
+        private int stepsPorog = 30;  //необходимо подобрать
 
         public bool isCrash(Car self)
         {
+            double deltaX = self.X - oldX;
+            double deltaY = self.Y - oldY;
+            double delta = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+
+            if (delta < deltaPorog)
+            {
+                steps++;
+            }else
+            {
+                steps = 0;
+                oldX = self.X;
+                oldY = self.Y;
+            }
+
+            if (steps>stepsPorog)
+            {
+                return true;
+            }
             return false;
+        }
+
+        public void CrashedMove()
+        {
+
         }
     }
 }
--- Regulator.cs
+++ Regulator.cs
@@ -8,9 +8,25 @@ namespace Com.CodeGame.CodeRacing2015.DevKit.CSharpCgdk
 {
     class Regulator
     {
+        private double errOld=0.0D;
+        private double I = 0.0D;
         public double getU(int X, int Y, Car self)
         {
-            return 0.0D;
+            double kp = 1.0D; //необходимо подобрать
+            double kd = 0.1D; //необходимо подобрать
+            double ki = 0.1D; //необходимо подобрать
+            double dt = 0.001;
+
+            double err = self.GetAngleTo(X, Y);
+            double P = err * kp;
+            double D = kd * (err - errOld)/dt;
+            I = I + ki * err*dt;
+
+            double U = P + I + D;
+
+            errOld = err;
+
+            return U;
         }
     }
 }
