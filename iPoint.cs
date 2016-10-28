4482f146897754e2b1c298b2cdfe6b9ecedbd6fe
SERG <SERG@SERG->
2016-10-28 20:58:42 +0400

    Написал самый примитивный расстановщик точек//Мужик сказал-мужик сделал!

--- iPoint.cs
+++ iPoint.cs
@@ -8,10 +8,23 @@ namespace Com.CodeGame.CodeRacing2015.DevKit.CSharpCgdk
 {
     class iPoint
     {
-
-        public int[] getNextPointXY(List<Tile> tiles, World world, Game game, Car self)
+        double k = 0.3;//Коэффицент смещения от центра тайла Мах=0.5, Min=0;
+        public double[] getNextPointXY(List<Tile> tiles, World world, Game game, Car self)
         {
-            return new int[] { 0, 0 };
+            TileType t = tiles[0].type;
+            if ((tiles[0].X == tiles[2].X) || (tiles[0].Y == tiles[2].Y))
+            {
+                return new double[2] { (tiles[1].X - 0.5D) * game.TrackTileSize, (tiles[1].Y - 0.5D) * game.TrackTileSize };
+
+            }
+            else
+            {
+                double X; //= (tiles[1].X - 0.5D) * game.TrackTileSize;
+                double Y; //= (tiles[1].Y - 0.5D) * game.TrackTileSize;
+                X = (tiles[1].X - 0.5D) * game.TrackTileSize + (tiles[2].X - tiles[0].X) * k * game.TrackTileSize;
+                Y = (tiles[1].Y - 0.5D) * game.TrackTileSize + (tiles[2].Y - tiles[0].Y) * k * game.TrackTileSize;
+                return new double[2] { X, Y };
+            }
         }
     }
 }
