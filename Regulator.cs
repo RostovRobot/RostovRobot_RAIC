068ee15c04a680d61a0cf91c80f0e1019693afb7
OvsyannikovAU <ovsyannikov.a.u@gmail.com>
2016-10-29 20:02:23 +0300

    Слегка перестроил регулятор - вынес коэффициенты из метода getU

--- Regulator.cs
+++ Regulator.cs
@@ -10,13 +10,15 @@ namespace Com.CodeGame.CodeRacing2015.DevKit.CSharpCgdk
     {
         private double errOld=0.0D;
         private double I = 0.0D;
+        //пусть коэффициенты стоят вне метода
+        //при необходимости можно будет добавить методы, изменяющие коэффициенты
+        //(например, для разных карт или типов машин)
+        private double kp = 1.0D; //необходимо подобрать
+        private double kd = 0.1D; //необходимо подобрать
+        private double ki = 0.1D; //необходимо подобрать
+        private double dt = 0.001;
         public double getU(int X, int Y, Car self)
         {
-            double kp = 1.0D; //необходимо подобрать
-            double kd = 0.1D; //необходимо подобрать
-            double ki = 0.1D; //необходимо подобрать
-            double dt = 0.001;
-
             double err = self.GetAngleTo(X, Y);
             double P = err * kp;
             double D = kd * (err - errOld)/dt;
